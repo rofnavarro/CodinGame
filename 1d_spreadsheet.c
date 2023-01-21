a1c23c71f48f16231b5986cdbd0d6d467485a221
rofnavarro <rofnavarro@gmail.com>
2023-01-20 23:01:29 -0300

    Codingame - 1D Spreadseet (not finished)

--- 1d_spreadsheet.c
+++ 1d_spreadsheet.c
@@ -3,177 +3,162 @@
 #include <string.h>
 #include <stdbool.h>
 
- typedef struct s_operation
- {
-    char oper[6];
-    int y;
-    int x;
- } t_operation;
 
-int array_filter(char array[6])
+ // -----------------------------------STRUCTS--------------------------------------
+
+typedef struct  s_numbers
 {
-    int ret;
-    char dif[5];
-    int i;
+    long x;
+    long y;
+}   t_numbers;
 
-    if (array[0] == '_')
+typedef struct  s_operation
+{
+    char    oper[6];
+    int     answer;
+}   t_operation;
+
+typedef struct   s_program
+{
+    t_numbers   pair;
+    t_operation o_pair;
+}   t_program;
+
+
+ // -----------------------------------UTILS----------------------------------------
+
+int     array_filter(char arg[6])
+{
+    int     ret;
+    char    dif[5];
+
+    if (arg[0] == '_')
         ret = 0;
-    else if (array[0] != '$')
-        ret = atoi(array);
+    else if (arg[0] != '$')
+        ret = atoi(arg);
     else
     {
+        int i;
+
         i = 1;
         while (i < 6)
         {
-            dif[i-1] = array[i];
+            dif[i - 1] = arg[i];
             i++;
         }
         ret = atoi(dif);
-        if (ret < 0)
-            ret = ret - 100001;
-        else
-            ret = ret + 100001;
+        ret = ret + 1000000001;
     }
     return (ret);
 }
 
-int *calculate(t_operation **program, int *ref, int N)
+void    run_operation(t_program *program, int i)
 {
-    int ret;
-    int counter;
-
-    counter = 0;
-    while (counter < N - 1)
-    {
-        printf("%s\n", program[counter]->oper);
-        if (strcmp(program[counter]->oper, "VALUE") == 0)
-        {
-            if (program[counter]->x > 100000)
-                program[counter]->x = (ref[program[counter]->x - 100001]);
-            else if (program[counter]->x < -100000)
-                program[counter]->x = (ref[program[counter]->x + 100001]);
-            ret = program[counter]->x;
-        }
-        else if (strcmp(program[counter]->oper, "ADD") == 0)
-        {
-            if (program[counter]->x > 100000)
-            {
-                if (program[counter]->y > 100000)
-                    program[counter]->y = (ref[program[counter]->y - 100001]);
-                else if(program[counter]->y < 100000)
-                    program[counter]->y = (ref[program[counter]->y + 100001]);
-                ret = program[counter]->x + program[counter]->y;
-            }
-            else if (program[counter]->x < -100000)
-            {
-                program[counter]->x = ref[program[counter]->x + 100001];
-                if (program[counter]->y > 100000)
-                    program[counter]->y = ref[program[counter]->y - 100001];
-                else if (program[counter]->y < -100000)
-                    program[counter]->y = ref[program[counter]->y + 100001];
-                ret = program[counter]->x + program[counter]->y;
-            }
-            else
-            {
-                if (program[counter]->y > 100000)
-                    program[counter]->y = (ref[program[counter]->y - 100001]);
-                else if (program[counter]->y < -100000)
-                    program[counter]->y = (ref[program[counter]->y + 100001]);
-                ret = program[counter]->x + program[counter]->y;
-            }
-        }
-        else if (strcmp(program[counter]->oper, "SUB") == 0)
-        {
-            if (program[counter]->x > 100000)
-            {
-                if (program[counter]->y > 100000)
-                    program[counter]->y = (ref[program[counter]->y - 100001]);
-                else if(program[counter]->y < 100000)
-                    program[counter]->y = (ref[program[counter]->y + 100001]);
-                ret = program[counter]->x - program[counter]->y;
-            }
-            else if (program[counter]->x < -100000)
-            {
-                program[counter]->x = ref[program[counter]->x + 100001];
-                if (program[counter]->y > 100000)
-                    program[counter]->y = ref[program[counter]->y - 100001];
-                else if (program[counter]->y < -100000)
-                    program[counter]->y = ref[program[counter]->y + 100001];
-                ret = program[counter]->x - program[counter]->y;
-            }
-            else
-            {
-                if (program[counter]->y > 100000)
-                    program[counter]->y = (ref[program[counter]->y - 100001]);
-                else if (program[counter]->y < -100000)
-                    program[counter]->y = (ref[program[counter]->y + 100001]);
-                ret = program[counter]->x - program[counter]->y;
-            }
-        }
-        else if (strcmp(program[counter]->oper, "MULT") == 0)
-        {
-            if (program[counter]->x > 100000)
-            {
-                if (program[counter]->y > 100000)
-                    program[counter]->y = (ref[program[counter]->y - 100001]);
-                else if(program[counter]->y < 100000)
-                    program[counter]->y = (ref[program[counter]->y + 100001]);
-                ret = program[counter]->x * program[counter]->y;
-            }
-            else if (program[counter]->x < -100000)
-            {
-                program[counter]->x = ref[program[counter]->x + 100001];
-                if (program[counter]->y > 100000)
-                    program[counter]->y = ref[program[counter]->y - 100001];
-                else if (program[counter]->y < -100000)
-                    program[counter]->y = ref[program[counter]->y + 100001];
-                ret = program[counter]->x * program[counter]->y;
-            }
-            else
-            {
-                if (program[counter]->y > 100000)
-                    program[counter]->y = (ref[program[counter]->y - 100001]);
-                else if (program[counter]->y < -100000)
-                    program[counter]->y = (ref[program[counter]->y + 100001]);
-                ret = program[counter]->x * program[counter]->y;
-            }
-        }
-        ref[counter] = ret;
-        counter++;
-    }
-    return (ref);
+    if (strcmp(program[i].o_pair.oper, "VALUE") == 0)
+        program[i].o_pair.answer = program[i].pair.x;
+    else if (strcmp(program[i].o_pair.oper, "ADD") == 0)
+        program[i].o_pair.answer = program[i].pair.x + program[i].pair.y;
+    else if (strcmp(program[i].o_pair.oper, "SUB") == 0)
+        program[i].o_pair.answer = program[i].pair.x - program[i].pair.y;
+    else if (strcmp(program[i].o_pair.oper, "MULT") == 0)
+        program[i].o_pair.answer = program[i].pair.x * program[i].pair.y;
 }
 
-int main()
+int     derref_value(t_program *program, int i)
 {
-    int         N, i;
-    int         *ret;
-    t_operation *program;
+    int ref;
+
+    if (program[i].pair.x > 1000000000)
+    {
+        ref = program[i].pair.x - 1000000001;
+        if (program[ref].pair.x < 1000000000 && program[ref].pair.y < 1000000000)
+        {
+            program[i].pair.x = program[ref].o_pair.answer; 
+            return (0);
+        }
+    }
+    if (program[i].pair.y > 1000000000)
+    {
+        ref = program[i].pair.y - 1000000001;
+        if (program[ref].pair.y < 1000000000 && program[ref].pair.y < 1000000000)
+        {
+            program[i].pair.y = program[ref].o_pair.answer; 
+            return (0);
+        }
+    }
+    return (1);
+}
+
+void    run_program(t_program *program, int size)
+{
+    int i, start;
+    int finished;
+    int derref;
+
+    finished = 1;
+    i = 0;
+    while (i < size)
+    {
+        if (program[i].pair.x <= 1000000000 || program[i].pair.y <= 1000000000)
+            break;
+        i++;
+    }
+    while (finished != 0)
+    {
+        while (i < size)
+        {
+            if (program[i].pair.x > 1000000000 || program[i].pair.y > 1000000000)
+            {
+                derref = derref_value(program, i);
+                if (derref == 0)
+                    run_operation(program, i);
+            }
+            else
+                run_operation(program, i);
+        i++;
+        }
+        for (int j = 0; j < size; j++)
+        {
+            if (program[j].pair.x > 1000000000 || program[i].pair.y > 1000000000)
+                i = 0;
+            else
+                finished = 0;
+        }
+
+    }
+}
+
+
+ // -----------------------------------MAIN-----------------------------------------
+
+int     main()
+{
+    int         N;
+    t_program   *program;
 
     scanf("%d", &N);
-    program = (t_operation *)malloc(sizeof(t_operation) * N);
-    ret = (int *)malloc(sizeof(int) * N);
-    
-    for (i = 0; i < N; i++) 
+    program = (t_program *)malloc(sizeof(t_program) * N);
+
+    for (int i = 0; i < N; i++)
     {
         char operation[6];
         char arg_1[7];
         char arg_2[7];
         scanf("%s%s%s", operation, arg_1, arg_2);
-        ret[i] = 0;
 
-        for (int j  = 0; j < strlen(operation); j++)
-        {
-            program[i].oper[j] = operation[j];
-        }
-        program[i].x = array_filter(arg_1);
-        program[i].y = array_filter(arg_2);
-        //printf("\n%s,  %d,   %d\n", program[i].oper, program[i].x, program[i].y);
+        strcpy(program[i].o_pair.oper, operation);
+        program[i].pair.x = array_filter(arg_1);
+        program[i].pair.y = array_filter(arg_2);
     }
     
-    i = 0;
+    run_program(program, N);
 
-    ret = calculate(&program, ret, N);
 
-    return (0);
+    for (int i = 0; i < N; i++)
+    {
+        printf("%d\n", program[i].o_pair.answer);
+    }
+
+    free(program);
+    return 0;
 }
